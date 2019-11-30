e1edb804a1453e8f998f3d2b924db4419846c160
addonnossosonho <56034707+addonnossosonho@users.noreply.github.com>
2019-11-30 13:16:12 -0300

    TESTE NOVO

--- plugin.video.PIRATES/core.py
+++ plugin.video.PIRATES/core.py
@@ -1,1 +1,1 @@
-[{"url": "https://raw.githubusercontent.com/BUILDTONYWARLLEY/TONYWARLLEY/master/VIEWS", "fanart": ".\\fanart.jpg", "title": "CANAIS"}]
+[{"url": "https://raw.githubusercontent.com/addonnossosonho/nossosonho/master/entrada", "fanart": ".\\fanart.jpg", "title": "CANAIS"}]
